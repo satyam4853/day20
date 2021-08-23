5ceb95d8231ffedfa9f5220ece96c66aca55250c
Your Name <you@example.com>
2021-08-23 15:07:35 +0530

    welcome Message

---
+++ MoodAnalysesWithCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalysesWithCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------This Is MoodAnalyses Problem-------------");
+        }
+    }
+}
