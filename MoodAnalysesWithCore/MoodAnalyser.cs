84d317a0ad6a46d874ea584f95da33d2169bdc98
Your Name <you@example.com>
2021-08-23 15:14:17 +0530

    feat:checking Mood Happy or Sad

---
+++ MoodAnalysesWithCore/MoodAnalyser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalysesWithCore
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        /// <summary>
+        /// Parameterised Constructor
+        /// </summary>
+        /// <param name="message"></param>
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyserMethod()
+        {
+            if (this.message.Contains("sad"))
+                return "Sad";
+            else
+                return "Happy";
+        }
+    }
+}
--- MoodAnalyzerCheck/UnitTest1.cs
+++ MoodAnalyzerCheck/UnitTest1.cs
@@ -1,13 +1,35 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalysesWithCore;
 
 namespace MoodAnalyzerCheck
 {
     [TestClass]
     public class UnitTest1
     {
+
+        MoodAnalyser moodAnalyser;
+        [DataRow("I am in sad mood")]
         [TestMethod]
-        public void TestMethod1()
+        public void Testfunction(string message)
         {
+            //Arrange
+            moodAnalyser = new MoodAnalyser(message);
+            //Act
+            var actual = moodAnalyser.AnalyserMethod();
+            //Assert
+            Assert.AreEqual("Sad", actual);
+        }
+
+        [DataRow("I am in any mood")]
+        [TestMethod]
+        public void Testfunctin(string message)
+        {
+            //Arrange
+            moodAnalyser = new MoodAnalyser(message);
+            //Act
+            var actual = moodAnalyser.AnalyserMethod();
+            //Assert
+            Assert.AreEqual("Happy", actual);
         }
     }
 }
